7c05f6cc9206d6356730b1da6a5ff1cc70c846c1
Reategui Agustin <131702509+QueenNixu@users.noreply.github.com>
2024-01-28 16:42:49 -0300

    Update ConexionMySQL.java

--- src/taekwondo/persistencia/ConexionMySQL.java
+++ src/taekwondo/persistencia/ConexionMySQL.java
@@ -8,7 +8,7 @@ public class ConexionMySQL {
     
     private static final String URL = "jdbc:mysql://localhost:3306/escuela_taekwondo";
     private static final String USER = "root";
-    private static final String PASSWORD = "4223";
+    private static final String PASSWORD = "1234";
     
     private static Connection conexion;
 
